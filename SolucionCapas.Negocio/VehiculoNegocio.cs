3f8de906338b2b2c25838dc2e5dd91f6efd78e22
micaelaaa1 <micaela.benites0777@gmail.com>
2026-08-25 15:50:33 -0300

    Add files via upload

---
+++ SolucionCapas.Negocio/VehiculoNegocio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SolucionCapas.Datos;
+
+namespace SolucionCapas.Negocio
+{
+    public class Vehiculo
+    {
+        public string Patente { get; set; }
+        public string Modelo { get; set; }
+        public bool TieneDeuda { get; set; }
+    }
+
+    public class VehiculoNegocio
+    {
+        private VehiculoDatos _datos = new VehiculoDatos();
+
+        public Vehiculo ObtenerVehiculo(string patente)
+        {
+            
+            if (string.IsNullOrEmpty(patente) || patente.Length < 6)
+                return null;
+
+            var resultado = _datos.BuscarPorPatente(patente);
+
+            if (resultado == null)
+                return null;
+
+            return new Vehiculo
+            {
+                Patente = resultado.Value.Patente,
+                Modelo = resultado.Value.Modelo,
+                TieneDeuda = resultado.Value.TieneDeuda
+            };
+        }
+    }
+}
